eaa1ee2a11040624373a5e3604a5b23a5d5b3d08
Cemil <cemiltan1999@gmail.com>
2024-08-14 17:44:39 +0300

    Config update

--- app.config.js
+++ app.config.js
@@ -30,15 +30,15 @@ export default {
       "favicon": "./assets/favicon.png"
     },
     "extra": {
-      "apiKey": process.env.API_KEY,
-      "authDomain": process.env.AUTH_DOMAIN,
-      "projectId": process.env.PROJECT_ID,
-      "storageBucket": process.env.STORAGE_BUCKET,
-      "messagingSenderId": process.env.MESSAGING_SENDER_ID,
-      "appId": process.env.APP_ID,
-      "measurementId": process.env.MEASUREMENT_ID,
+      "apiKey": process.env.EXPO_PUBLIC_API_KEY,
+      "authDomain": process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+      "projectId": process.env.EXPO_PUBLIC_PROJECT_ID,
+      "storageBucket": process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+      "messagingSenderId": process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+      "appId": process.env.EXPO_PUBLIC_APP_ID,
+      "measurementId": process.env.EXPO_PUBLIC_MEASUREMENT_ID,
       "eas": {
-        "projectId": process.env.EAS_PROJECT_ID
+        "projectId": process.env.EXPO_PUBLIC_EAS_PROJECT_ID
       }
     }
   }
